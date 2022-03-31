3369932256a064f382087faed0c886d32e38eafe
KevinPilatzke <pilatzke_k@hotmail.com>
2022-03-31 16:50:43 -0500

    v2 Fizzbuzz and 2 more other shells

--- Shell/FizzBuzz.sh
+++ Shell/FizzBuzz.sh
@@ -7,22 +7,25 @@
     -Print nothing if the number isn't divisible by 3 or 5
 -Create the file by name fizzbuzz.sh"
 
-re='-?[0-9]+'
 echo "Hello! Would you like to play a game?"
-echo "Pick a number from 1 to 20"
-read -p "Option: " Option
+while true
+do
+    echo "Pick a number from 1 to 20 [Type quit to exit game]"
+    read -p "Option: " Option
 
-if ! [[ $Option =~ $re ]]
-    then echo "Not a number" >&2; exit 1
-elif (( 21 <= $Option >= 1 ))
-    then echo "Not a number between 1 and 20"
-elif (( $Option == 0))
-    then echo "Not a number between 1 and 20"
-elif (( $Option == 15 ))
-    then echo "FizzBuzz"
-elif (( $Option % 3 == 0 ))
-    then echo "Fizz"
-elif (( $Option % 5 == 0))
-    then echo "Buzz"
-else echo "Nothing"
-fi
+    if [[ $Option = "quit" ]]
+        then break
+    elif (( 21 > $Option < 1 ))
+        then echo "Not a number between 1 and 20"
+    elif (( $Option <= 0 ))
+        then echo "Not a number between 1 and 20"
+    elif (( $Option == 15 ))
+        then echo "FizzBuzz"
+    elif (( $Option % 3 == 0 ))
+        then echo "Fizz"
+    elif (( $Option % 5 == 0 ))
+        then echo "Buzz"
+    else echo "Nothing"
+    fi
+done
+echo "Thanks for playing!"
