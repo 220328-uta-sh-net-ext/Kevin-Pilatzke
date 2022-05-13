8dde0cdc1cb3307d584a9baafc70e313f236dbbd
KevinPilatzke <pilatzke_k@hotmail.com>
2022-05-13 16:22:56 -0500

    API programs fixed

--- Projects/Restaurant(P1)/RestaurantAPI/Program.cs
+++ Projects/Restaurant(P1)/RestaurantAPI/Program.cs
@@ -1,23 +1,54 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using RestaurantBL;
+using RestaurantDL;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ConfigurationManager Config = builder.Configuration;
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(o => {
+    var key = Encoding.UTF8.GetBytes(Config["JWT:Key"]);
+    o.SaveToken = true;
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = Config["JWT:Issuer"],
+        ValidAudience = Config["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateLifetime = true,
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddMemoryCache();
+builder.Services.AddControllers(options =>
+    options.RespectBrowserAcceptHeader = true
+    )
+    .AddXmlSerializerFormatters();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IRepo>(repo => new SqlRepo(Config.GetConnectionString("RestaurantDb")));
+builder.Services.AddScoped<IAccountLogic, AccountLogic>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()|| app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
