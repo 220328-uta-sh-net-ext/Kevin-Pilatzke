f47416b37b76feb66f250af4cb70b50665d93644
KevinPilatzke <pilatzke_k@hotmail.com>
2022-05-13 14:59:11 -0500

    Trying Fix

---
+++ Projects/Restaurant(P1)/ResturantModels/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RestaurantModels
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string RestaurantName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string Details { get; set; } 
+
+        /// <summary>
+        /// Need to set Overide here so when Searching it will properly display the information we are looking for.
+        /// Format we Need used 4 of the 5 columns, RestaurantID is not needed for searching, just for Primary Key on Database
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string results = $"Name: {RestaurantName}\nCity: {City}\nState: {State}\nZipcode: {ZipCode}\nDetails: {Details}";
+            return results;
+        }
+    }
+}
